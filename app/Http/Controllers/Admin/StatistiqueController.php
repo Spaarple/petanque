8ccdb36935409f1da6d7d769451b327f9d46abc9
Louka <Louka06.millon@gmail.com>
2024-01-28 18:18:08 +0100

    Add new charts for unique visitors, user registrations, and tournament registrations

--- app/Http/Controllers/Admin/StatistiqueController.php
+++ app/Http/Controllers/Admin/StatistiqueController.php
@@ -10,13 +10,15 @@ use App\Models\TrafficSource;
 use App\Models\DailyVisit;
 use Carbon\Carbon;
 use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use App\Models\User;
+use App\Models\ParticipationTournois;
 
 
 class StatistiqueController extends Controller
 {
     public function index()
     {
-        $chart_options = [
+        $uniqueVisitorsChartOptions = [
             'chart_title' => 'Visiteurs uniques par jour',
             'report_type' => 'group_by_date',
             'model' => 'App\Models\DailyVisit',
@@ -30,7 +32,7 @@ class StatistiqueController extends Controller
             'continuous_time' => true,
             'aggregate_function' => 'sum',
             'aggregate_field' => 'unique_visitors', // Aggregate by 'unique_visitors'
-            'chart_height' => '300px',
+            'chart_height' => '100px',
             'color' => '#0000ff', // Set the color of the chart line to blue
             'conditions' => [
                 [
@@ -41,8 +43,60 @@ class StatistiqueController extends Controller
                 ],
             ],
         ];
-        $chart1 = new LaravelChart($chart_options);
-        
+        $uniqueVisitorsChart = new LaravelChart($uniqueVisitorsChartOptions);
+
+        $userRegistrationChartOptions = [
+            'chart_title' => 'Inscriptions par Jour',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\User',
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'day',
+            'chart_type' => 'line',
+            'chart_color' => '#00c292',
+            'filter_field' => 'created_at',
+            'filter_days' => 30,
+            'chart_height' => '100px',
+            'aggregate_function' => 'count',
+            'aggregate_field' => 'id',
+            'conditions' => [
+                [
+                    'name' => 'Visiteurs',
+                    'condition' => 'created_at > -1', // SQL condition
+                    'color' => 'blue',
+                    'fill' => true,
+                ],
+            ],
+        ];
+        $userRegistrationChart = new LaravelChart($userRegistrationChartOptions);
+        // chart for all inscriptions tournois
+        $inscriptionsTournoisChartOptions = [
+            'chart_title' => 'Inscriptions Tournois par Jour',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\ParticipationTournois',
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'day',
+            'chart_type' => 'line',
+            'chart_color' => '#00c292',
+            'filter_field' => 'created_at',
+            'filter_days' => 30,
+            'chart_height' => '100px',
+            'aggregate_function' => 'count',
+            'aggregate_field' => 'id',
+            'conditions' => [
+                [
+                    'name' => 'Visiteurs',
+                    'condition' => 'created_at > -1', // SQL condition
+                    'color' => 'blue',
+                    'fill' => true,
+                ],
+            ],
+            
+            
+        ];
+
+        $inscriptionsTournoisChart = new LaravelChart($inscriptionsTournoisChartOptions);
+
+
         $ipViews = IpView::orderBy('views', 'desc')->take(10)->get();
         $pageViews = PageView::orderBy('views', 'desc')->take(10)->get();
         $trafficSources = TrafficSource::orderBy('hits', 'desc')->take(10)->get();
@@ -52,6 +106,14 @@ class StatistiqueController extends Controller
             ->orderBy('date')
             ->get(['date', 'visits', 'unique_visitors']);
 
-        return view('admin.statistiques.index', compact('ipViews', 'pageViews', 'trafficSources', 'dailyVisits', 'chart1'));
+        return view('admin.statistiques.index', compact(
+            'ipViews',
+            'pageViews',
+            'trafficSources',
+            'dailyVisits',
+            'uniqueVisitorsChart',
+            'userRegistrationChart',
+            'inscriptionsTournoisChart'
+        ));
     }
 }
--- resources/views/admin/statistiques/index.blade.php
+++ resources/views/admin/statistiques/index.blade.php
@@ -6,106 +6,137 @@
             <div class="p-6">
                 <h2 class="text-lg font-semibold text-gray-700">Statistiques</h2>
 
-                {{-- Daily Visits Chart --}}
-                <div class="mt-6">
-                    <h3 class="text-md font-semibold text-gray-600">Visites Journalières</h3>
-                    {!! $chart1->renderHtml() !!}
+                <div class="flex flex-wrap -mx-4">
+                    {{-- Daily Visits Chart --}}
+                    <div class="w-full md:w-1/3 px-4 mb-6">
+                        <h3 class="text-md font-semibold text-gray-600">Visites Journalières</h3>
+                        <div class="bg-white p-4 rounded-lg shadow">
+                            {!! $uniqueVisitorsChart->renderHtml() !!}
+                        </div>
+                    </div>
 
+                    {{-- Register user per day --}}
+                    <div class="w-full md:w-1/3 px-4 mb-6">
+                        <h3 class="text-md font-semibold text-gray-600">Inscriptions Journalières</h3>
+                        <div class="bg-white p-4 rounded-lg shadow">
+                            {!! $userRegistrationChart->renderHtml() !!}
+                        </div>
+                    </div>
 
+                    {{-- Another Chart or Table --}}
+                    <div class="w-full md:w-1/3 px-4 mb-6">
+                        <h3 class="text-md font-semibold text-gray-600">Nombre d'inscription total aux tournois</h3>
+                        <div class="bg-white p-4 rounded-lg shadow">
+                            {{-- Inscription au tournois via le site --}}
+                            {!! $inscriptionsTournoisChart->renderHtml() !!}
+                        </div>
+                    </div>
                 </div>
 
-                {{-- IP Views --}}
-                <div class="mt-6">
-                    <h3 class="text-md font-semibold text-gray-600">Top IP Views</h3>
-                    <table class="min-w-full mt-2">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    IP</th>
-                                <th
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Views</th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($ipViews as $view)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $view->ip }}
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $view->views }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                <div class="flex flex-wrap -mx-4">
+                    {{-- IP Views --}}
+                    <div class="w-full lg:w-1/2 px-4 mb-6">
+                        <h3 class="text-md font-semibold text-gray-600">Top IP Views</h3>
+                        <div class="bg-white p-4 rounded-lg shadow">
+                            {{-- IP Views --}}
+                            <table class="min-w-full mt-2">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            IP</th>
+                                        <th
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Views</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach ($ipViews as $view)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                                {{ $view->ip }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{ $view->views }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    {{-- Page Views --}}
+                    <div class="w-full lg:w-1/2 px-4 mb-6">
+                        <h3 class="text-md font-semibold text-gray-600">Top Page Views</h3>
+                        <div class="bg-white p-4 rounded-lg shadow">
+                            {{-- Table for Page Views --}}
+                            <table class="min-w-full mt-2">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Page</th>
+                                        <th
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Views</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach ($pageViews as $view)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $view->page }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $view->views }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
 
-                {{-- Page Views --}}
-                <div class="mt-6">
-                    <h3 class="text-md font-semibold text-gray-600">Top Page Views</h3>
-                    <table class="min-w-full mt-2">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Page</th>
-                                <th
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Views</th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($pageViews as $view)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $view->page }}
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $view->views }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-
-                {{-- Traffic Sources --}}
-                <div class="mt-6">
-                    <h3 class="text-md font-semibold text-gray-600">Top Traffic Sources</h3>
-                    <table class="min-w-full mt-2">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Source</th>
-                                <th
-                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Hits</th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($trafficSources as $source)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $source->referer }}
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $source->hits }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                <div class="flex flex-wrap -mx-4">
+                    {{-- Traffic Sources --}}
+                    <div class="w-full px-4">
+                        <h3 class="text-md font-semibold text-gray-600">Top Traffic Sources</h3>
+                        <div class="bg-white p-4 rounded-lg shadow">
+                            {{-- Table for Traffic Sources --}}
+                            <table class="min-w-full mt-2">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Source</th>
+                                        <th
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Hits</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    @foreach ($trafficSources as $source)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $source->referer }}
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $source->hits }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
 
             </div>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
     @section('javascript')
-        {!! $chart1->renderChartJsLibrary() !!}
-        {!! $chart1->renderJs() !!}
+        {!! $uniqueVisitorsChart->renderChartJsLibrary() !!}
+        {!! $uniqueVisitorsChart->renderJs() !!}
+        {!! $userRegistrationChart->renderJs() !!}
+        {!! $inscriptionsTournoisChart->renderJs() !!}
     @endsection
-
-
-
 </x-app-layout>
