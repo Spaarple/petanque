b10fd78f4d486ea0d5571b325f838f951fd94f83
Louka <Louka06.millon@gmail.com>
2024-02-21 22:08:25 +0100

    Refactor sponsor card layout in index.blade.php

--- resources/views/users/sponsors/index.blade.php
+++ resources/views/users/sponsors/index.blade.php
@@ -18,31 +18,28 @@
                 </div>
             @endif
             @foreach ($sponsors as $sponsor)
-                <div class="p-2 w-1/2 md:w-1/4 lg:w-1/6">
-                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+            <div class="p-2 md:w-1/2 lg:w-1/4 sm:w-full w-full">
+                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow w-58>
+                    <a href="/sponsors/{{ $sponsor->id }}">
+                        <!-- Ajout des classes w-32 et h-32 pour définir une taille fixe de 128px par 128px -->
+                        <img src="{{ asset('storage/' . $sponsor->sponsor_logo) }}" alt="{{ $sponsor->sponsor_name }}" class="w-32 h-32 object-cover rounded-t-lg">
+                    </a>
+                    <div class="p-5">
                         <a href="/sponsors/{{ $sponsor->id }}">
-                            <img src="{{ asset('storage/' . $sponsor->sponsor_logo) }}" alt="{{ $sponsor->sponsor_name }}"
-                                class="rounded-t-lg">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {{ $sponsor->sponsor_name }}</h5>
                         </a>
-                        <div class="p-5">
-                            <a href="/sponsors/{{ $sponsor->id }}">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    {{ $sponsor->sponsor_name }}</h5>
-                            </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $sponsor->sponsor_description }}
-                            </p>
-                            <a href="/sponsors/{{ $sponsor->id }}"
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Voir
-                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
-                        </div>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $sponsor->sponsor_description }}</p>
+                        <a href="/sponsors/{{ $sponsor->id }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Voir
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
                     </div>
                 </div>
+            </div>
+            
             @endforeach
         </div>
     </div>
