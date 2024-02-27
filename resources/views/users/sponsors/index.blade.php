f5cae1e016090f8852474b77adaae6940bbc99d4
Louka <Louka06.millon@gmail.com>
2024-02-27 21:08:44 +0100

    Fix sponsor description truncation in sponsors index view

--- resources/views/users/sponsors/index.blade.php
+++ resources/views/users/sponsors/index.blade.php
@@ -25,7 +25,7 @@
                             </a>
                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-ellipsis overflow-hidden h-20">
                                 {{-- truncate 30 words--}}
-                                {{ $sponsor->sponsor_description>words(30) }}
+                                {{ $sponsor->sponsor_description->words(30) }}
                             </p>
                             <a href="/sponsors/{{ $sponsor->id }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                                 Voir
