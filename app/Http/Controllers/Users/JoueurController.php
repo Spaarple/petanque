c14d447dc40b1f138c7822d6a2ff8aace38218ee
Louka <Louka06.millon@gmail.com>
2024-02-27 19:02:48 +0100

    Refactor club filter in JoueurController

--- app/Http/Controllers/Users/JoueurController.php
+++ app/Http/Controllers/Users/JoueurController.php
@@ -10,9 +10,13 @@ class JoueurController extends Controller
 {
     public function index()
     {
-        $users = User::where('club', 'ASC ESCOUBLAC')
+        // si club = "ASCE ESCOUBLAC" ou ASCE Section Petanque
+        $users = User::where('club', 'ASCE ESCOUBLAC') 
+                     ->orWhere('club', 'ASCE Section Petanque')
                      ->where('is_approved', 1)
                      ->get(['first_name','last_name', 'email', 'profile_photo_path']);
+
+        
         
         return view('users.joueurs.index', compact('users'));
     }
