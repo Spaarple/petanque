9522fc67233c7d9cbbe3a04d327c5c96d708ef03
Louka <Louka06.millon@gmail.com>
2024-02-27 21:19:06 +0100

    Add class attribute to application logo image

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('/storage/' . config('site.logo_path')) }}" alt="Application Logo">
+<img src="{{ asset('/storage/' . config('site.logo_path')) }}" alt="Application Logo" class="h-10 w-auto">
