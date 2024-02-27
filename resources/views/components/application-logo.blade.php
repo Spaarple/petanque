d1180a88615369f184e466b87b4348a91ea1490d
Louka <Louka06.millon@gmail.com>
2024-02-27 21:19:27 +0100

    Update application logo size

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('/storage/' . config('site.logo_path')) }}" alt="Application Logo" class="h-10 w-auto">
+<img src="{{ asset('/storage/' . config('site.logo_path')) }}" alt="Application Logo" class="h-24 w-auto">
